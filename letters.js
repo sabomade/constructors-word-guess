1db5d5297cd800c02a67deb0a8bec4737e53fe71
Sara Bolduc <sara.s.bolduc@gmail.com>
2019-12-30 21:18:34 -0800

    adds letters.js - is working!

---
+++ letters.js
@@ -0,0 +1,30 @@
+//Constructor function
+var Letter = function(char, guessed){
+    this.char = char;
+    this.guessed = guessed;
+}
+
+//function to display letter or _ based on this.guessed
+Letter.prototype.toString = function(){
+    if (this.guessed){
+        return this.char+ " ";
+    }
+    return "_ ";
+}    
+//update this.guessed to true if user guess is correct
+Letter.prototype.guess = function(userGuess){
+    if(this.char === userGuess){
+        return this.guessed = true;
+    }
+    return this.guessed = false;
+}
+
+//export Letter consturctor, required in word.js
+module.exports = Letter;
+
+//test case - yes this works!
+// const d = new Letter("d", false);
+// console.log(d+"");
+// console.log(d.guess("a"));
+// console.log(d.guess("d"));
+// console.log(d+"");
